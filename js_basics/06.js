844a0ea12db5f9204db346c19ef4cd3cb2da8fda
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2024-08-26 09:51:42 +0000

    chapter 6

---
+++ js_basics/06.js
@@ -0,0 +1,9 @@
+let num="33abc"
+let num1=Number(num)
+//console.log(typeof(num))
+//console.log(num)//
+//console.log(typeof(num1))
+//console.log(num)
+let value=0
+let boolean=Boolean(value)
+console.log(boolean)
