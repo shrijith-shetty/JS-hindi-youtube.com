b3a8dc913242de69e8463cd6f5a47c6dc3d33264
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2024-08-27 13:45:06 +0000

    data type summary

---
+++ DATA_TYPES_JAVASCRIPT/09_data_type.js
@@ -0,0 +1,26 @@
+const value=Symbol('123') //Symbol(123)
+//console.log(value)
+const score=100
+const islog=false
+const id=Symbol('123')
+//console.log(value==id)  //fale
+//console.log(value===id)
+const integer=651345613213213453132n
+//console.log(typeof integer) //bigint
+
+//non premetive data types
+
+const  heroes=["shaktiman","doga"];
+
+const myobj = {               //object
+    nm : "hithesh",
+    age : 22,
+};
+
+ 
+const myfunction = function() {
+    console.log("hello");
+}
+
+
+console.log(myfunction)
