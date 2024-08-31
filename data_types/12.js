e8792ac0567cea1805194db8dd444fa550e69086
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2024-08-31 14:32:04 +0000

    maths and number

---
+++ data_types/12.js
@@ -0,0 +1,37 @@
+const n=100;
+//console.log(n);
+
+const n1=new Number(150);
+//console.log(n1);
+
+//console.log(n1.toString());
+//console.log(n1.toString().length);
+
+//console.log(n.toFixed(10));
+
+const prec=123.74324;
+
+//console.log(prec.toPrecision(3));
+
+const hundres= 10000000;
+
+//console.log(hundres.toLocaleString('en-In'));
+
+
+//_______________MATH_________________________
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(5.9));
+// console.log(Math.max(5,3,4,3,56,6));     //to comment click ctrl+k and ctrl+c
+// console.log(Math.min(1,5,2,6,2,6,));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+const min=10;
+const max=100;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
