8d81a0c61de2aeafa7b66d8db6985e788ee59cf8
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2024-08-31 13:24:28 +0000

    Pending changes exported from your codespace

---
+++ data_types/12.js
@@ -0,0 +1,14 @@
+const n=100;
+console.log(n);
+
+const n1=new Number(150);
+console.log(n1);
+
+console.log(n1.toString());
+console.log(n1.toString().length);
+
+console.log(n.toFixed(10));
+
+const prec=123.74324;
+
+console.log(prec.toPrecision(3));
